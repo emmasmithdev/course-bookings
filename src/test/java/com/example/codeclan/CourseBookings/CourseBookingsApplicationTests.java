c7405fc26bfac0e80ea30d8dfb9a1a9d146c700e
Emma <emmagray183@gmail.com>
2020-02-13 13:21:19 +0000

    mvp tests passing

--- src/test/java/com/example/codeclan/CourseBookings/CourseBookingsApplicationTests.java
+++ src/test/java/com/example/codeclan/CourseBookings/CourseBookingsApplicationTests.java
@@ -1,10 +1,30 @@
 package com.example.codeclan.CourseBookings;
 
+import com.example.codeclan.CourseBookings.models.Booking;
+import com.example.codeclan.CourseBookings.models.Course;
+import com.example.codeclan.CourseBookings.models.Customer;
+import com.example.codeclan.CourseBookings.repositories.BookingRepository;
+import com.example.codeclan.CourseBookings.repositories.CourseRepository;
+import com.example.codeclan.CourseBookings.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CourseBookingsApplicationTests {
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
 
 	@Test
 	void contextLoads() {
@@ -12,7 +32,26 @@ class CourseBookingsApplicationTests {
 
 	@Test
 	public void getAllCoursesGivenRating(){
+		List<Course> found = courseRepository.findByStars(5);
+		assertEquals(1, found.size());
+	}
 
+	@Test
+	public void getAllCustomersForGivenCourse(){
+		List<Customer> found = customerRepository.findByBookingsCourseId(1L);
+		assertEquals(2, found.size());
+	}
+
+	@Test
+	public void getAllCoursesForGivenCustomer(){
+		List<Course> found = courseRepository.findByBookingsCustomerId(1L);
+		assertEquals(3, found.size());
+	}
+
+	@Test
+	public void getAllBookingsForGivenDate(){
+		List<Booking> found = bookingRepository.findByDate("01/01/20");
+		assertEquals(1, found.size());
 	}
 
 }
